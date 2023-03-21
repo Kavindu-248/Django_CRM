5e24d531ce63118411de3ba806d7c56791ca9585
Kavindu Fernando <kavindu2331@gmail.com>
2023-03-21 22:01:48 +0530

    feat: added abastract user

--- leads/models.py
+++ leads/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+
+
+class User(AbstractUser):
+    pass
+    
+
 class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
 
