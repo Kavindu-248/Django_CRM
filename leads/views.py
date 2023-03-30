280c0b0286a0811304f8e53d5cac6eb0e5cd7006
Kavindu Fernando <kavindu2331@gmail.com>
2023-03-30 16:59:14 +0530

    fix: changed varaible name

--- leads/views.py
+++ leads/views.py
@@ -4,11 +4,11 @@ from .models import Lead
 
 # Create your views here.
 
-def home_page(request):
+def lead_list(request):
     leads = Lead.objects.all()
     context = {
         "leads": leads
     }
     # return HttpResponse("Hello World")
-    return render(request, "second_page.html", context)
+    return render(request, "leads/lead_list.html", context)
 
