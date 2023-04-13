9eea9ee01c3dc36935ad14d909a5251e8627915a
Kavindu Fernando <kavindu2331@gmail.com>
2023-04-13 13:36:12 +0530

    feat: set up registration views

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,7 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path,include
-from leads.views import landing_page, LandingPageView
+from leads.views import landing_page, LandingPageView, SignupView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -10,6 +11,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path( '', LandingPageView.as_view(), name='landing-page'),
     path('leads/', include('leads.urls', namespace='leads')),
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 
 ]
 
