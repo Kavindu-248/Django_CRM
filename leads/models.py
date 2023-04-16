3a1f26040b85e07aeb19901b4b1dbe69c8c977b4
Kavindu Fernando <kavindu2331@gmail.com>
2023-04-16 23:11:55 +0530

    feat: changes to mdoels

--- leads/models.py
+++ leads/models.py
@@ -1,10 +1,10 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
     pass
-
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
