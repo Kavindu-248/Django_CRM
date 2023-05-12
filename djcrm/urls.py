b6feb0115e340a2d37e2f96e306591ac03006901
Kavindu Fernando <kavindu2331@gmail.com>
2023-05-12 22:05:32 +0400

    feat: added password reset

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path,include
 from leads.views import landing_page, LandingPageView, SignupView
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView,PasswordResetView
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -11,7 +11,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path( '', LandingPageView.as_view(), name='landing-page'),
     path('leads/', include('leads.urls', namespace='leads')),
+    path('agents/', include('agents.urls', namespace='agents')),
     path('signup/', SignupView.as_view(), name='signup'),
+    path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 
