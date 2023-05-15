8c1e4c1e9f6917a5f8b2691bb9696e0776a62cf3
Kavindu Fernando <kavindu2331@gmail.com>
2023-05-15 23:55:42 +0400

    fix: minor changes

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -1,5 +1,10 @@
 
 from pathlib import Path
+import environ
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
