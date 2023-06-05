a225a3e373e08b0b76796c54614eddf0cb6a3bba
Kavindu Fernando <kavindu2331@gmail.com>
2023-06-05 14:00:36 +0400

    fix: changes to the model.py

--- leads/models.py
+++ leads/models.py
@@ -21,6 +21,7 @@ class Lead(models.Model):
     age = models.IntegerField(default=0)
     organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     agent = models.ForeignKey ("Agent", null=True , blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey("Category", related_name="leads", null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
@@ -43,8 +44,9 @@ post_save.connect(post_user_created_signal, sender=User)
 
 
 class Catergory(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30) #New,Attempted,Contacted,Converted
     organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
 
     def __str__(self):
         return self.name
